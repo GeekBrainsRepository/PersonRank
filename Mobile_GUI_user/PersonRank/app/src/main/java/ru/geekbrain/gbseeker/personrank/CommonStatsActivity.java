04942176ec2f9423836d754be1036a79192e066e
Mikhail Doroshenko <domiur@yandex.ru>
2017-01-21 23:19:47 +0300

    переименовал MainActivity->CommonStatsActivity

--- Mobile_GUI_user/PersonRank/app/src/main/java/ru/geekbrain/gbseeker/personrank/CommonStatsActivity.java
+++ Mobile_GUI_user/PersonRank/app/src/main/java/ru/geekbrain/gbseeker/personrank/CommonStatsActivity.java
@@ -10,7 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+public class CommonStatsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
