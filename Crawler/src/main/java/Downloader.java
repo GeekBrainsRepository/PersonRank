984b9a29731f943d94b50ee17011f9c97f2ff045
viktor <vik_ivy@mail.ru>
2017-01-22 22:22:57 +0500

    рефакторинг

--- Crawler/src/main/java/Downloader.java
+++ Crawler/src/main/java/Downloader.java
@@ -16,18 +16,15 @@ public class Downloader {
     private Document htmlDocument;
 
     public List<String> download(String url){
-        try{
-
+        try {
             Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
             Document htmlDocument = connection.get();
             this.htmlDocument = htmlDocument;
 
             if(connection.response().statusCode() == 200){
-
                 System.out.println("\n**Посещаю** Полученную веб-страницу на " + url);
             }
             if(!connection.response().contentType().contains("text/html")){
-
                 System.out.println("**Ошибка** Тип страницы отличается от HTML");
             }
 
@@ -35,19 +32,13 @@ public class Downloader {
             System.out.println("Найдено (" + linksOnPage.size() + ") ссылок");
 
             for(Element link : linksOnPage){
-
                 this.links.add(link.absUrl("href"));
-
             }
-
-
         }
         catch(IOException ioe){
-
            ioe.printStackTrace();
         }
         return links;
-     }
-
     }
+}
 
