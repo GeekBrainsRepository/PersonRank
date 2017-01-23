d1c782d31a0ea007373e3f15cbf453a3c5e8de25
Mikhail Doroshenko <domiur@mail.ru>
2017-01-23 13:01:27 +0300

    фикс перевода перевода строки

--- Mobile_GUI_user/PersonRank/app/src/main/java/ru/geekbrain/gbseeker/personrank/CommonStatsFragment.java
+++ Mobile_GUI_user/PersonRank/app/src/main/java/ru/geekbrain/gbseeker/personrank/CommonStatsFragment.java
@@ -46,3 +46,6 @@ public class CommonStatsFragment extends Fragment {
         return v;
     }
 }
+
+
+
--- Mobile_GUI_user/PersonRank/app/src/main/java/ru/geekbrain/gbseeker/personrank/MainActivity.java
+++ Mobile_GUI_user/PersonRank/app/src/main/java/ru/geekbrain/gbseeker/personrank/MainActivity.java
@@ -96,3 +96,6 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 }
+
+
+
