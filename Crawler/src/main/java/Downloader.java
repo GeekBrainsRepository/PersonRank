21702a21b955021be753ec792796d83df00a203a
viktor <vik_ivy@mail.ru>
2017-01-23 10:45:16 +0500

    рефакторинг

--- Crawler/src/main/java/Downloader.java
+++ Crawler/src/main/java/Downloader.java
@@ -15,30 +15,29 @@ public class Downloader {
     private List<String> links = new LinkedList<String>();
     private Document htmlDocument;
 
-    public List<String> download(String url){
+    public List<String> download(String url) {
         try {
+
             Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
             Document htmlDocument = connection.get();
             this.htmlDocument = htmlDocument;
 
-            if(connection.response().statusCode() == 200){
+            if (connection.response().statusCode() == 200) {
                 System.out.println("\n**Посещаю** Полученную веб-страницу на " + url);
             }
-            if(!connection.response().contentType().contains("text/html")){
+            if (!connection.response().contentType().contains("text/html")) {
                 System.out.println("**Ошибка** Тип страницы отличается от HTML");
             }
 
             Elements linksOnPage = htmlDocument.select("a[href]");
             System.out.println("Найдено (" + linksOnPage.size() + ") ссылок");
 
-            for(Element link : linksOnPage){
+            for (Element link : linksOnPage) {
                 this.links.add(link.absUrl("href"));
             }
-        }
-        catch(IOException ioe){
-           ioe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
         return links;
     }
 }
-
