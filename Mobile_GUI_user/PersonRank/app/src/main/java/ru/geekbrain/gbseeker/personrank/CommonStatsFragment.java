a7bb615fc15ed62053a702c1aac4682513888f80
Mikhail Doroshenko <domiur@mail.ru>
2017-01-23 11:27:24 +0300

    добавил файлы для Андроид студио

---
+++ Mobile_GUI_user/PersonRank/app/src/main/java/ru/geekbrain/gbseeker/personrank/CommonStatsFragment.java
@@ -0,0 +1,48 @@
+package ru.geekbrain.gbseeker.personrank;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class CommonStatsFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View v=inflater.inflate(R.layout.common_stats,container,false);
+
+          String[] data = {"lenta.ru", "yandex.ru", "mail.ru", "google.ru"};
+            // адаптер
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, data);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            Spinner spinner = (Spinner) v.findViewById(R.id.common_stats_sites);
+            spinner.setAdapter(adapter);
+
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                    // показываем позиция нажатого элемента
+                    Toast.makeText(getActivity(), "Position = " + position, Toast.LENGTH_SHORT).show();
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> arg0) {
+                }
+            });
+
+        return v;
+    }
+}
