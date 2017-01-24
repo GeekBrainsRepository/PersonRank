5c066daaa02f81251883a2e7a26e27dfa06b467f
Volcano91 <volcano91@bk.ru>
2017-01-24 18:51:41 +0100

    Добавил классы, которые используются для в Downloader + парсер

---
+++ Crawler/src/main/java/other classes/parser/InvalidXmlException.java
@@ -0,0 +1,31 @@
+package geekbrains.internship.project_full.Crawler.src.main.java.other_classes.parser;
+
+/**
+ * An exception that indicates that a file that should be parsed is not an XML file.
+ */
+class InvalidXmlException extends RuntimeException {
+    /**
+     * Create a new InvalidXmlException with a message.
+     * @param message    The message of the exception.
+     */
+    public InvalidXmlException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Create a new InvalidXmlException with a message and a cause.
+     * @param message    The message of the exception.
+     * @param cause      The cause of the exception.
+     */
+    public InvalidXmlException(final String message, final Exception cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Create a new InvalidXmlException with a cause.
+     * @param cause    The cause of the exception.
+     */
+    public InvalidXmlException(final Exception cause) {
+        super(cause);
+    }
+}
---
+++ Crawler/src/main/java/other classes/parser/PlaintextSitemapParser.java
@@ -0,0 +1,76 @@
+package geekbrains.internship.project_full.Crawler.src.main.java.other_classes.parser;
+
+
+import geekbrains.internship.project_full.Crawler.src.main.java.other_classes.parser.model.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+
+class PlaintextSitemapParser implements ISitemapParser {
+    private static final Pattern URL_PATTERN = Pattern.compile("https?://[A-Za-z0-9\\-\\._~:/\\?#\\[\\]@!\\$&'\\(\\)\\*\\+,;=%]+");
+    private static final int INVALID_LINES_ALLOWED = 3;
+    private final String urlPrefix;
+    private final InputStream inputStream;
+    private Set<SitemapEntry> sitemapEntries;
+    private boolean validUrlFound = false;
+
+    public PlaintextSitemapParser(String urlPrefix, InputStream inputStream) {
+        this.urlPrefix = urlPrefix;
+        this.inputStream = inputStream;
+    }
+
+    @Override
+    public void parse() {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        sitemapEntries = new LinkedHashSet<>();
+        String line;
+        int lineCount = 0;
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                lineCount++;
+                line = line.trim();
+                if (line.isEmpty()) {
+                    continue;
+                }
+                if (!validUrlFound) {
+                    if (isUrl(line)) {
+                        validUrlFound = true;
+                    } else if (lineCount >= INVALID_LINES_ALLOWED) {
+                        throw new SitemapParseException("Sitemap seems to be invalid. Please make sure that this is a valid sitemap.");
+                    }
+                }
+                if (line.startsWith(urlPrefix)) {
+                    sitemapEntries.add(new SitemapEntry(line));
+                }
+            }
+        } catch (IOException e) {
+            throw new UrlConnectionException(e.getMessage());
+        }
+    }
+
+    private boolean isUrl(String text) {
+        return URL_PATTERN.matcher(text).matches();
+    }
+
+    @Override
+    public Set<SitemapIndex> getSitemapIndexes() {
+        return new LinkedHashSet<>(0);
+    }
+
+    @Override
+    public Set<SitemapEntry> getSitemapEntries() {
+        return sitemapEntries;
+    }
+
+    @Override
+    public Sitemap.SitemapType getSitemapType() {
+        return Sitemap.SitemapType.TEXT;
+    }
+}
