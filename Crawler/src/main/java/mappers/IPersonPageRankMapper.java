ff9c1d65cddce49e8bbfa6b5bbce01565b6d5e00
viktor <vik_ivy@mail.ru>
2017-01-24 22:01:19 +0500

    добавил основу мапперов

--- Crawler/src/main/java/mappers/IPersonPageRankMapper.java
+++ Crawler/src/main/java/mappers/IPersonPageRankMapper.java
@@ -1,7 +1,18 @@
 package mappers;
 
+import beans.PersonPageRank;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
 /**
  * mapper for table PersonPageRank
  */
 public interface IPersonPageRankMapper {
+
+    String SELECT_PERSON_PAGE_RANK = "SELECT person_id as personId, page_id as pageId," +
+            "rank FROM person_page_rank";
+
+    @Select(SELECT_PERSON_PAGE_RANK)
+    List<PersonPageRank> getPersonPageRank();
 }
--- Crawler/src/main/java/mappers/ISitesMapper.java
+++ Crawler/src/main/java/mappers/ISitesMapper.java
@@ -1,7 +1,18 @@
 package mappers;
 
+import beans.Sites;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
 /**
  * mapper for Sites table
  */
 public interface ISitesMapper {
+
+    String SELECT_SITES = "SELECT id, site_name as siteName FROM sites";
+
+    @Select(SELECT_SITES)
+    List<Sites> getSites();
+
 }
