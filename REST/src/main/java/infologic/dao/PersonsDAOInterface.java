b4b2c20066234970a2da9e070766e639a8958abf
lWeRl <werasdas@gmail.com>
2017-01-26 14:14:58 +0300

    PersonsDAOInterface minimal method for common statistic

---
+++ REST/src/main/java/infologic/dao/PersonsDAOInterface.java
@@ -0,0 +1,9 @@
+package infologic.dao;
+
+import java.util.Collection;
+
+import infologic.model.PersonsEntity;
+
+public interface PersonsDAOInterface {
+	Collection<PersonsEntity> getAllPerson();
+}
