4013164323d3b2472231e89a60b3756fb6646112
lWeRl <werasdas@gmail.com>
2017-01-26 12:32:20 +0300

    Add UserRepository interface. Add Common and Daily object. It's answer  by user request. Add FakeUserRepository.

---
+++ REST/src/main/java/infologic/repository/Common.java
@@ -0,0 +1,24 @@
+package infologic.repository;
+
+import java.util.Date;
+import java.util.Map;
+
+public class Common {
+
+	private Date date;
+	private Map<String, Integer> result;
+
+	public Common(Date date, Map<String, Integer> result) {
+		this.result = result;
+		this.date = date;
+	}
+
+	public Map<String, Integer> getResult() {
+		return result;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+}
---
+++ REST/src/main/java/infologic/repository/Daily.java
@@ -0,0 +1,15 @@
+package infologic.repository;
+
+import java.util.List;
+
+public class Daily {
+	List<Integer> result;
+
+	public Daily(List<Integer> result) {
+		this.result = result;
+	}
+
+	public List<Integer> getResult() {
+		return result;
+	}
+}
