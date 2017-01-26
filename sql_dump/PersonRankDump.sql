1713f4ea498af3ff481ed6a9363c97c34c3f9fd4
oleglomako <lom-o@yandex.ru>
2017-01-26 17:41:03 +0100

    Привел название полей на соответствие архитектуре в файле дампа базы данных

--- sql_dump/PersonRankDump.sql
+++ sql_dump/PersonRankDump.sql
@@ -1,6 +1,10 @@
--- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
+DROP DATABASE IF EXISTS `PersonRankDB`;
+CREATE DATABASE  IF NOT EXISTS `PersonRankDB`; 
+USE `PersonRankDB`;
+
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
--- Host: localhost    Database: personrank
+-- Host: localhost    Database: PersonRankDB
 -- ------------------------------------------------------
 -- Server version	5.7.17
 
@@ -16,139 +20,138 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `keywords`
+-- Table structure for table `Keywords`
 --
-
-DROP TABLE IF EXISTS `keywords`;
+DROP TABLE IF EXISTS `Keywords`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `keywords` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор ключевого слова',
-  `name` varchar(2048) NOT NULL COMMENT 'Название ключевого слова',
-  `person_id` int(11) NOT NULL COMMENT 'Идентификатор личности, \nкоторой соответствует данное ключевое слово.\n\nЯвляется внешним ключом к таблице Persons.',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `KeywordsPersonFK_idx` (`person_id`),
-  CONSTRAINT `KeywordsPersonFK` FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `Keywords` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор ключевого слова',
+  `Name` varchar(2048) NOT NULL COMMENT 'Название ключевого слова',
+  `PersonID` int(11) NOT NULL COMMENT 'Идентификатор личности, \nкоторой соответствует данное ключевое слово.\n\nЯвляется внешним ключом к таблице Persons.',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`),
+  KEY `KeywordsPersonFK_IDx` (`PersonID`),
+  CONSTRAINT `KeywordsPersonFK` FOREIGN KEY (`PersonID`) REFERENCES `Persons` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `keywords`
+-- Dumping data for table `Keywords`
 --
 
-LOCK TABLES `keywords` WRITE;
-/*!40000 ALTER TABLE `keywords` DISABLE KEYS */;
-INSERT INTO `keywords` VALUES (1,'Путин',1),(2,'Путина',1),(3,'Путину',1),(4,'Путиным',1),(5,'Медведев',2),(6,'Медведева',2),(7,'Медведеву',2),(8,'Медведевым',2),(9,'Навальный',3),(10,'Навального',3),(11,'Навальному',3),(12,'Навальным',3),(13,'Пупкин',5),(14,'Пупкина',5),(15,'Пупкину',5);
-/*!40000 ALTER TABLE `keywords` ENABLE KEYS */;
+LOCK TABLES `Keywords` WRITE;
+/*!40000 ALTER TABLE `Keywords` DISABLE KEYS */;
+INSERT INTO `Keywords` VALUES (1,'Путин',1),(2,'Путина',1),(3,'Путину',1),(4,'Путиным',1),(5,'Медведев',2),(6,'Медведева',2),(7,'Медведеву',2),(8,'Медведевым',2),(9,'Навальный',3),(10,'Навального',3),(11,'Навальному',3),(12,'Навальным',3),(13,'Пупкин',5),(14,'Пупкина',5),(15,'Пупкину',5);
+/*!40000 ALTER TABLE `Keywords` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `pages`
+-- Table structure for table `Pages`
 --
 
-DROP TABLE IF EXISTS `pages`;
+DROP TABLE IF EXISTS `Pages`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `pages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор страницы сайта',
-  `url` varchar(2048) NOT NULL COMMENT 'Полный URL адрес страницы',
-  `site_id` int(11) NOT NULL COMMENT 'Идентификатор сайта (ресурса), \nкоторый предоставлен  администратором для анализа. \n\nЯвляется внешним ключом к таблице Sites',
-  `found_data_time` datetime NOT NULL COMMENT 'Дата и время обнаружения страницы системой',
-  `last_scan_date` datetime NOT NULL COMMENT 'Дата и время последней проверки на упоминания',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `PagesSitesFK_idx` (`site_id`),
-  CONSTRAINT `PagesSiteFK` FOREIGN KEY (`site_id`) REFERENCES `sites` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `Pages` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор страницы сайта',
+  `Url` varchar(2048) NOT NULL COMMENT 'Полный Url адрес страницы',
+  `SiteID` int(11) NOT NULL COMMENT 'Идентификатор сайта (ресурса), \nкоторый предоставлен  администратором для анализа. \n\nЯвляется внешним ключом к таблице Sites',
+  `FoundDateTime` datetime NOT NULL COMMENT 'Дата и время обнаружения страницы системой',
+  `LastScanDate` datetime NOT NULL COMMENT 'Дата и время последней проверки на упоминания',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`),
+  KEY `PagesSitesFK_IDx` (`SiteID`),
+  CONSTRAINT `PagesSiteFK` FOREIGN KEY (`SiteID`) REFERENCES `Sites` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `pages`
+-- Dumping data for table `Pages`
 --
 
-LOCK TABLES `pages` WRITE;
-/*!40000 ALTER TABLE `pages` DISABLE KEYS */;
-INSERT INTO `pages` VALUES (1,'http://lenta.ru/rubrics/world/',1,'2010-10-20 15:15:23','2003-04-20 15:15:23');
-/*!40000 ALTER TABLE `pages` ENABLE KEYS */;
+LOCK TABLES `Pages` WRITE;
+/*!40000 ALTER TABLE `Pages` DISABLE KEYS */;
+INSERT INTO `Pages` VALUES (1,'http://lenta.ru/rubrics/world/',1,'2010-10-20 15:15:23','2003-04-20 15:15:23');
+/*!40000 ALTER TABLE `Pages` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `person_page_rank`
+-- Table structure for table `PersonPageRank`
 --
 
-DROP TABLE IF EXISTS `person_page_rank`;
+DROP TABLE IF EXISTS `PersonPageRank`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `person_page_rank` (
-  `person_id` int(11) DEFAULT NULL COMMENT 'Идентификатор личности, \nкоторой соответствует данное ключевое слово. \n\nЯвляется внешним ключом к таблице Persons.',
-  `page_id` int(11) DEFAULT NULL COMMENT 'Идентификатор страницы сайта, \nна которой найдены упоминания о персонах. \n\nЯвляется внешним ключом к таблице Pages\n',
-  `rank` int(11) DEFAULT NULL COMMENT 'Количество упоминаний личности на странице ',
-  KEY `PersonPersonsPageRankFK_idx` (`person_id`),
-  KEY `PagePersonPageRankFK_idx` (`page_id`),
-  CONSTRAINT `PagePersonPageRankFK` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `PersonPersonsPageRankFK` FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `PersonPageRank` (
+  `PersonID` int(11) DEFAULT NULL COMMENT 'Идентификатор личности, \nкоторой соответствует данное ключевое слово. \n\nЯвляется внешним ключом к таблице Persons.',
+  `PageID` int(11) DEFAULT NULL COMMENT 'Идентификатор страницы сайта, \nна которой найдены упоминания о персонах. \n\nЯвляется внешним ключом к таблице Pages\n',
+  `Rank` int(11) DEFAULT NULL COMMENT 'Количество упоминаний личности на странице ',
+  KEY `PersonPersonsPageRankFK_IDx` (`PersonID`),
+  KEY `PagePersonPageRankFK_IDx` (`PageID`),
+  CONSTRAINT `PagePersonPageRankFK` FOREIGN KEY (`PageID`) REFERENCES `Pages` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `PersonPersonsPageRankFK` FOREIGN KEY (`PersonID`) REFERENCES `Persons` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `person_page_rank`
+-- Dumping data for table `PersonPageRank`
 --
 
-LOCK TABLES `person_page_rank` WRITE;
-/*!40000 ALTER TABLE `person_page_rank` DISABLE KEYS */;
-INSERT INTO `person_page_rank` VALUES (1,1,2);
-/*!40000 ALTER TABLE `person_page_rank` ENABLE KEYS */;
+LOCK TABLES `PersonPageRank` WRITE;
+/*!40000 ALTER TABLE `PersonPageRank` DISABLE KEYS */;
+INSERT INTO `PersonPageRank` VALUES (1,1,2);
+/*!40000 ALTER TABLE `PersonPageRank` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `persons`
+-- Table structure for table `Persons`
 --
 
-DROP TABLE IF EXISTS `persons`;
+DROP TABLE IF EXISTS `Persons`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `persons` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор личности',
-  `name` varchar(2048) NOT NULL COMMENT 'Наименование личности',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+CREATE TABLE `Persons` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор личности',
+  `Name` varchar(2048) NOT NULL COMMENT 'Наименование личности',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `persons`
+-- Dumping data for table `Persons`
 --
 
-LOCK TABLES `persons` WRITE;
-/*!40000 ALTER TABLE `persons` DISABLE KEYS */;
-INSERT INTO `persons` VALUES (1,'Путин'),(2,'Медведев'),(3,'Навальный'),(5,'Пупкин');
-/*!40000 ALTER TABLE `persons` ENABLE KEYS */;
+LOCK TABLES `Persons` WRITE;
+/*!40000 ALTER TABLE `Persons` DISABLE KEYS */;
+INSERT INTO `Persons` VALUES (1,'Путин'),(2,'Медведев'),(3,'Навальный'),(5,'Пупкин');
+/*!40000 ALTER TABLE `Persons` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `sites`
+-- Table structure for table `Sites`
 --
 
-DROP TABLE IF EXISTS `sites`;
+DROP TABLE IF EXISTS `Sites`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sites` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор сайта',
-  `name` varchar(2048) NOT NULL COMMENT 'Наименование сайта',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+CREATE TABLE `Sites` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор сайта',
+  `Name` varchar(2048) NOT NULL COMMENT 'Наименование сайта',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `sites`
+-- Dumping data for table `Sites`
 --
 
-LOCK TABLES `sites` WRITE;
-/*!40000 ALTER TABLE `sites` DISABLE KEYS */;
-INSERT INTO `sites` VALUES (1,'lenta.ru');
-/*!40000 ALTER TABLE `sites` ENABLE KEYS */;
+LOCK TABLES `Sites` WRITE;
+/*!40000 ALTER TABLE `Sites` DISABLE KEYS */;
+INSERT INTO `Sites` VALUES (1,'lenta.ru');
+/*!40000 ALTER TABLE `Sites` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -160,4 +163,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-01-19 14:37:33
+-- Dump completed on 2017-01-26 17:23:01
