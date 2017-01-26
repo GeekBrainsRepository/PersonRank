4bccbd6f380687bddf86f3e9d4829a104d9c9e93
lWeRl <werasdas@gmail.com>
2017-01-26 12:33:07 +0300

    Comment old method. Add new base on UserRepository

--- REST/src/main/java/infologic/controller/StatController.java
+++ REST/src/main/java/infologic/controller/StatController.java
@@ -1,16 +1,16 @@
 package infologic.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import infologic.repository.Common;
+import infologic.repository.Daily;
+import infologic.repository.FakeUserRepository;
+import infologic.repository.UserRepository;
 
 /**
  * Created by Антон Владимирович on 21.01.2017.
@@ -18,58 +18,75 @@ import java.util.Map;
 @RestController
 public class StatController {
 
-    @RequestMapping("/statistic/getpersonlist")
-    public Map<Integer,String> getPersonList() {
+	UserRepository repository = new FakeUserRepository(); // создаем репозиторий
+															// пока фейковый
 
+	@RequestMapping("/statistic/getpersonlist")
+	public Map<Integer, String> getPersonList() {
 
-        //fake data
-        //TODO replace with DAO request
-        Map<Integer,String> map = new HashMap<>();
-        map.put(1,"First");
-        map.put(2,"Second");
+		// fake data
+		// TODO replace with DAO request
+		Map<Integer, String> map = new HashMap<>();
+		map.put(1, "First");
+		map.put(2, "Second");
 
+		return map;
 
-        return map;
+	}
 
-    }
-    @RequestMapping("/statistic/getresourcelist")
-    public Map<Integer,String> getResourceList() {
+	@RequestMapping("/statistic/getresourcelist")
+	public Map<Integer, String> getResourceList() {
 
-        //fake data
-        //TODO replace with DAO request
-        Map<Integer,String> map = new HashMap<>();
-        map.put(1,"lenta.ru");
-        map.put(2,"meduza.io");
+		// fake data
+		// TODO replace with DAO request
+		Map<Integer, String> map = new HashMap<>();
+		map.put(1, "lenta.ru");
+		map.put(2, "meduza.io");
 
-        return map;
+		return map;
 
-    }
-    @RequestMapping("/statistic/common/{resourceId}")
-    public Map<String,Integer> getCommonResourceStat(@PathVariable("resourceId") Integer resourceId) {
-        //fake data
-        //TODO replace with DAO request
-        Map<String,Integer> map = new HashMap<>();
-        map.put("Putin",450);
-        map.put("Medvedev",370);
-        map.put("Variable",resourceId);
+	}
+	// @RequestMapping("/statistic/common/{resourceId}")
+	// public Map<String,Integer>
+	// getCommonResourceStat(@PathVariable("resourceId") Integer resourceId) {
+	// //fake data
+	// //TODO replace with DAO request
+	// Map<String,Integer> map = new HashMap<>();
+	// map.put("Putin",450);
+	// map.put("Medvedev",370);
+	// map.put("Variable",resourceId);
+	//
+	//
+	// return map;
 
+	// }
 
-        return map;
+	// Финальный вид для реализованного интрфейса
+	@RequestMapping("/statistic/common/{siteId}")
+	public Common getCommon(@PathVariable("siteId") Integer siteId) {
+		return repository.get(siteId);
+	}
 
-    }
-    @RequestMapping("/statistic/daily/{resourceId}/{personId}/{dateStart}/{dateEnd}")
-    public Map<LocalDate,Integer> getDailyStat(@PathVariable Integer resourceId,@PathVariable Integer personId,
-                                          @PathVariable String dateStart,@PathVariable String  dateEnd) {
-        Map<LocalDate,Integer> map = new HashMap<>();
+	// @RequestMapping("/statistic/daily/{resourceId}/{personId}/{dateStart}/{dateEnd}")
+	// public Map<LocalDate,Integer> getDailyStat(@PathVariable Integer
+	// resourceId,@PathVariable Integer personId,
+	// @PathVariable String dateStart,@PathVariable String dateEnd) {
+	// Map<LocalDate,Integer> map = new HashMap<>();
+	//
+	// //String parsing example
+	// DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd:MM:yyyy");
+	// LocalDate dateTime = LocalDate.parse(dateStart, formatter);
+	// Date.from(dateTime.atStartOfDay(ZoneId.systemDefault()).toInstant());
+	//
+	// map.put(dateTime,1000);
+	// map.put(dateTime,1001);
+	//
+	// return map;
+	// }
 
-        //String parsing example
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd:MM:yyyy");
-        LocalDate dateTime = LocalDate.parse(dateStart, formatter);
-        Date.from(dateTime.atStartOfDay(ZoneId.systemDefault()).toInstant());
-
-        map.put(dateTime,1000);
-        map.put(dateTime,1001);
-
-        return map;
-    }
+	// Финальный вид для реализованного интрфейса
+	@RequestMapping("/statistic/daily/{siteId}/{personId}/{dateStart}/{dateEnd}")
+	public Daily getDaily(@PathVariable Integer siteId, @PathVariable Integer personId, @PathVariable Long dateStart, @PathVariable Long dateEnd) {
+		return repository.get(siteId, personId, dateStart, dateEnd);
+	}
 }
