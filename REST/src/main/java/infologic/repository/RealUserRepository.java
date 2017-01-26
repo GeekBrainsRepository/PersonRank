bf20795aa4a6ed4ace5a5da7d33f5fbaa861fbdd
lWeRl <werasdas@gmail.com>
2017-01-26 14:16:23 +0300

    RealUserRepository. Add get common statistic implementation.

---
+++ REST/src/main/java/infologic/repository/RealUserRepository.java
@@ -0,0 +1,45 @@
+package infologic.repository;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import infologic.dao.PagesDAOInterface;
+import infologic.dao.PersonsDAOInterface;
+import infologic.dao.PersonsPageRankDAOInterface;
+import infologic.model.PersonPageRankEntity;
+import infologic.model.PersonsEntity;
+
+public class RealUserRepository implements UserRepository {
+
+	PersonsDAOInterface persons;
+	PersonsPageRankDAOInterface personsPageRank;
+	PagesDAOInterface pages;
+
+	@Override
+	public Common get(int siteId) {
+		Map<String, Integer> result = new HashMap();
+		Date date = new Date(0);
+		ArrayList<PersonsEntity> personList = new ArrayList<>(persons.getAllPerson());
+		for (PersonsEntity o : personList) {
+			String name = o.getName();
+			ArrayList<PersonPageRankEntity> rankList = new ArrayList<>(personsPageRank.getPersonPageRankByPersonSite(o.getId(), siteId));
+			int sum = 0;
+			for (PersonPageRankEntity ppr : rankList) {
+				sum += ppr.getRank();
+				Date temp = pages.getById(ppr.getPageId()).getLastScanDate();
+				if (temp.after(date))
+					date = temp;
+			}
+		}
+		return new Common(date, result);
+	}
+
+	@Override
+	public Daily get(int siteId, int personId, long dateStart, long dateEnd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
