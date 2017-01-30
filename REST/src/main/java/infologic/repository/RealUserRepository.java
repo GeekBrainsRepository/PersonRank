ce592bb39bf1ffc1b0716afc08b391c50e5a882d
lWeRl <werasdas@gmail.com>
2017-01-30 11:53:55 +0300

    Закоментировано все тело, файл более не нужен. Так же чтоб собрать проект

--- REST/src/main/java/infologic/repository/RealUserRepository.java
+++ REST/src/main/java/infologic/repository/RealUserRepository.java
@@ -13,34 +13,34 @@ import java.util.Map;
 
 public class RealUserRepository implements UserRepository {
 
-	PersonsDAOInterface persons;
-	PersonsPageRankDAOInterface personsPageRank;
-	PagesDAOInterface pages;
-
-	@Override
-	public Common get(int siteId) {
-		Map<String, Integer> result = new HashMap();
-		Date date = new Date(0);
-		ArrayList<PersonsEntity> personList = new ArrayList<>(persons.getAllPerson());
-		for (PersonsEntity o : personList) {
-			String name = o.getName();
-			ArrayList<PersonPageRankEntity> rankList = new ArrayList<>(personsPageRank.getPersonPageRankByPersonSite(o.getId(), siteId));
-			int sum = 0;
-			for (PersonPageRankEntity ppr : rankList) {
-				sum += ppr.getId();
-				Date temp = pages.getById(ppr.getPageId()).getLastScanDate();
-				if (temp.after(date))
-					date = temp;
-			}
-			result.put(name, sum);
-		}
-		return new Common(date, result);
-	}
-
-	@Override
-	public Daily get(int siteId, int personId, long dateStart, long dateEnd) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+//	PersonsDAOInterface persons;
+//	PersonsPageRankDAOInterface personsPageRank;
+//	PagesDAOInterface pages;
+//
+//	@Override
+//	public Common get(int siteId) {
+//		Map<String, Integer> result = new HashMap();
+//		Date date = new Date(0);
+//		ArrayList<PersonsEntity> personList = new ArrayList<>(persons.getAllPerson());
+//		for (PersonsEntity o : personList) {
+//			String name = o.getName();
+//			ArrayList<PersonPageRankEntity> rankList = new ArrayList<>(personsPageRank.getPersonPageRankByPersonSite(o.getId(), siteId));
+//			int sum = 0;
+//			for (PersonPageRankEntity ppr : rankList) {
+//				sum += ppr.getId();
+//				Date temp = pages.getById(ppr.getPageId()).getLastScanDate();
+//				if (temp.after(date))
+//					date = temp;
+//			}
+//			result.put(name, sum);
+//		}
+//		return new Common(date, result);
+//	}
+//
+//	@Override
+//	public Daily get(int siteId, int personId, long dateStart, long dateEnd) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 
 }
