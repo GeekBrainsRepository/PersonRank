d33962e27969764d98954ac53fd3631f0759e809
froghub <anton.unixxx@gmail.com>
2017-01-30 12:34:41 +0500

    renamed and replaced

---
+++ REST/src/main/java/infologic/model/DailyStat.java
@@ -0,0 +1,15 @@
+package infologic.model;
+
+import java.util.List;
+
+public class DailyStat {
+	List<Integer> result;
+
+	public DailyStat(List<Integer> result) {
+		this.result = result;
+	}
+
+	public List<Integer> getResult() {
+		return result;
+	}
+}
