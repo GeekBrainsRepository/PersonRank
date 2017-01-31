a0235f58c7484d4c2214214459e209828d2050bf
Е. Мартынов <. @ADMIN-VG2PI2JRQ.Dlink>
2017-01-31 16:35:02 +0200

    Добавил механизм взаимодействия с REST-сервисом (получение общей статистики). Добавил в класс GeneralStatisticOnSite поле reviewDate (дата последней проверки сайта). Добавил библиотеку для работы с json.

--- Desktop_GUI_user/src/ru/personrank/data/generalstatistic/GeneralStatisticOnSite.java
+++ Desktop_GUI_user/src/ru/personrank/data/generalstatistic/GeneralStatisticOnSite.java
@@ -3,6 +3,8 @@
  */
 package ru.personrank.data.generalstatistic;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -10,11 +12,13 @@ import java.util.List;
  */
 public class GeneralStatisticOnSite {
     private String siteName;
+    private Calendar reviewDate;
     private List<String> personNames;
     private List<Integer> allPersonRanks;
 
-    public GeneralStatisticOnSite(String siteName, List<String> personNames, List<Integer> allPersonRanks) {
+    public GeneralStatisticOnSite(String siteName,Calendar reviewDate, List<String> personNames, List<Integer> allPersonRanks) {
         this.siteName = siteName;
+        this.reviewDate = reviewDate;
         this.personNames = personNames;
         this.allPersonRanks = allPersonRanks;
     }
@@ -23,6 +27,10 @@ public class GeneralStatisticOnSite {
         return siteName;
     }
 
+    public Calendar getReviewDate() {
+        return reviewDate;
+    }
+    
     public List<String> getPersonNames() {
         return personNames;
     }
--- Desktop_GUI_user/src/ru/personrank/data/generalstatistic/GeneralStatisticOnSiteRepository.java
+++ Desktop_GUI_user/src/ru/personrank/data/generalstatistic/GeneralStatisticOnSiteRepository.java
@@ -3,67 +3,108 @@
  */
 package ru.personrank.data.generalstatistic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import ru.personrank.data.Repository;
 import ru.personrank.data.Specification;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import org.json.JSONObject;
 
 /**
  *
  */
 public class GeneralStatisticOnSiteRepository implements Repository<GeneralStatisticOnSite> {
 
+    private static final String URL_GET_SITE_MAP = "http://37.194.87.95:30000/statistic/getresourcelist";
+    private static final String URL_GET_GENERAL_STATISTIC_ON_SITE = "http://37.194.87.95:30000/statistic/common/";
+    
     private static final GeneralStatisticOnSiteRepository INSTANCE = new GeneralStatisticOnSiteRepository();
 
-    ArrayList<GeneralStatisticOnSite> generalStatisticOnSite;
+    List<GeneralStatisticOnSite> generalStatisticOnSite;
 
     private GeneralStatisticOnSiteRepository() {
         generalStatisticOnSite = new ArrayList<>();
-        // Тестовые данные
-        // Начало
-        String siteName1 = "lenta.ru";
-        ArrayList<String> persons1 = new ArrayList<>();
-        persons1.add("Путин");
-        persons1.add("Трамп");
-        persons1.add("Обама");
-        persons1.add("Меркиль");
-        persons1.add("Оланд");
-        persons1.add("Навальный");
-        persons1.add("Жириновский");
-        ArrayList<Integer> allRanks1 = new ArrayList<>();
-        allRanks1.add(1227);
-        allRanks1.add(1820);
-        allRanks1.add(987);
-        allRanks1.add(681);
-        allRanks1.add(5787);
-        allRanks1.add(57);
-        allRanks1.add(121);
-        generalStatisticOnSite.add(new GeneralStatisticOnSite(siteName1, persons1, allRanks1));
-
-        String siteName2 = "komersant.ru";
-        ArrayList<String> persons2 = new ArrayList<>();
-        persons2.add("Путин");
-        persons2.add("Трамп");
-        persons2.add("Обама");
-        persons2.add("Меркиль");
-        persons2.add("Оланд");
-        persons2.add("Навальный");
-        persons2.add("Жириновский");
-        ArrayList<Integer> allRanks2 = new ArrayList<>();
-        allRanks2.add(1147);
-        allRanks2.add(1745);
-        allRanks2.add(1001);
-        allRanks2.add(791);
-        allRanks2.add(670);
-        allRanks2.add(100);
-        allRanks2.add(189);
-        generalStatisticOnSite.add(new GeneralStatisticOnSite(siteName2, persons2, allRanks2));
-        // Конец
+        Iterator<Map.Entry<String, Object>> entries = getSiteMap().entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry<String, Object> entry = entries.next();
+            generalStatisticOnSite.add(new GeneralStatisticOnSite(
+                    (String) entry.getValue(),
+                    getReviewDate(entry.getKey()),
+                    getPersonsList(entry.getKey()),
+                    getRanksList(entry.getKey())));
+        }
     }
 
     public static GeneralStatisticOnSiteRepository getInstance() {
         return INSTANCE;
+    }
+
+    // Метод возвращает обьект json по url адресу ресурса
+    private static JSONObject getJSON(String url) {
+        JSONObject json = null;
+        try {
+            URL source = new URL(url);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(source.openStream()));
+            String inputLine;
+            StringBuilder sb = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                sb.append(inputLine);
+            }
+            json = new JSONObject(sb.toString());
+            in.close();
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return json;
+    }
+
+    // Метод возвращает карту сайтов
+    private static Map<String, Object> getSiteMap() {
+        JSONObject json = getJSON(URL_GET_SITE_MAP);
+        return json.toMap();
+    }
+
+    // Метод возвращает дату последнего обновления статистики сайта
+    private static Calendar getReviewDate(String siteID) {
+        JSONObject json = getJSON(URL_GET_GENERAL_STATISTIC_ON_SITE + siteID);
+        GregorianCalendar date = new GregorianCalendar();
+        date.setTimeInMillis(json.getLong("date"));
+        return date;
+    }
+    
+    // Метод возвращает список персон сайта
+    private static List<String> getPersonsList (String siteID) {
+        List<String> persons = new ArrayList<>();
+        JSONObject json = getJSON(URL_GET_GENERAL_STATISTIC_ON_SITE + siteID);
+        JSONObject jsonResult = (JSONObject) json.get("result");
+        for (Map.Entry<String, Object> entry : jsonResult.toMap().entrySet()) {
+            persons.add((String)entry.getKey());
+        }
+        return persons;
+    }
+    
+    // Метод возвращает список рейтингов персон
+    private static List<Integer> getRanksList (String siteID) {
+        List<Integer> ranks = new ArrayList<>();
+        JSONObject json = getJSON(URL_GET_GENERAL_STATISTIC_ON_SITE + siteID);
+        JSONObject jsonResult = (JSONObject) json.get("result");
+        for (Map.Entry<String, Object> entry : jsonResult.toMap().entrySet()) {
+            ranks.add((Integer) entry.getValue());
+        }
+        return ranks;
     }
 
     @Override
@@ -92,4 +133,45 @@ public class GeneralStatisticOnSiteRepository implements Repository<GeneralStati
         return newList;
     }
 
+//    // Тестовые данные
+//        // Начало
+//        String siteName1 = "lenta.ru";
+//        ArrayList<String> persons1 = new ArrayList<>();
+//        persons1.add("Путин");
+//        persons1.add("Трамп");
+//        persons1.add("Обама");
+//        persons1.add("Меркиль");
+//        persons1.add("Оланд");
+//        persons1.add("Навальный");
+//        persons1.add("Жириновский");
+//        ArrayList<Integer> allRanks1 = new ArrayList<>();
+//        allRanks1.add(1227);
+//        allRanks1.add(1820);
+//        allRanks1.add(987);
+//        allRanks1.add(681);
+//        allRanks1.add(5787);
+//        allRanks1.add(57);
+//        allRanks1.add(121);
+//        generalStatisticOnSite.add(new GeneralStatisticOnSite(siteName1, persons1, allRanks1));
+//
+//        String siteName2 = "komersant.ru";
+//        ArrayList<String> persons2 = new ArrayList<>();
+//        persons2.add("Путин");
+//        persons2.add("Трамп");
+//        persons2.add("Обама");
+//        persons2.add("Меркиль");
+//        persons2.add("Оланд");
+//        persons2.add("Навальный");
+//        persons2.add("Жириновский");
+//        ArrayList<Integer> allRanks2 = new ArrayList<>();
+//        allRanks2.add(1147);
+//        allRanks2.add(1745);
+//        allRanks2.add(1001);
+//        allRanks2.add(791);
+//        allRanks2.add(670);
+//        allRanks2.add(100);
+//        allRanks2.add(189);
+//        generalStatisticOnSite.add(new GeneralStatisticOnSite(siteName2, persons2, allRanks2));
+//        // Конец
+//
 }
