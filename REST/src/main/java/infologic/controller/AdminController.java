d3f6e742fdb77e0b70634e5f3bf181d995091339
lWeRl <werasdas@gmail.com>
2017-01-31 12:40:13 +0300

    Админский котроллер

--- REST/src/main/java/infologic/controller/AdminController.java
+++ REST/src/main/java/infologic/controller/AdminController.java
@@ -1,21 +1,85 @@
 package infologic.controller;
 
+import infologic.model.Book;
+import infologic.model.KeywordsEntity;
 import infologic.model.PersonsEntity;
+import infologic.model.SitesEntity;
+import infologic.repository.BookRepository;
+import infologic.repository.Repository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AdminController {
 
+    private static Repository<Book> repository = new BookRepository();
 
+    ////////////////////////////////DELETE
     @RequestMapping(method = RequestMethod.DELETE, path = "/admin/person/{id}")
-    public @ResponseBody PersonsEntity deletePerson(@PathVariable Integer id){
-        //deleting user
-        PersonsEntity ent = new PersonsEntity();
-        ent.setName("testname");
-        ent.setId(666);
-        return ent;
-
+    public @ResponseBody void deletePerson(@PathVariable Integer id){
+        PersonsEntity pattern = new PersonsEntity();
+        pattern.setId(id);
+        repository.remove(pattern);
     }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/admin/site/{id}")
+    public @ResponseBody void deleteSite(@PathVariable Integer id){
+        SitesEntity pattern = new SitesEntity();
+        pattern.setId(id);
+        repository.remove(pattern);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/admin/keyword/{id}")
+    public @ResponseBody void deleteKeyWord(@PathVariable Integer id){
+        KeywordsEntity pattern = new KeywordsEntity();
+        pattern.setId(id);
+        repository.remove(pattern);
+    }
+    //////////////////////////////////PUT
+    @RequestMapping(method = RequestMethod.PUT, path = "/admin/person/{name}")
+    public @ResponseBody void putPerson(@PathVariable String name){
+        PersonsEntity pattern = new PersonsEntity();
+        pattern.setName(name);
+        repository.add(pattern);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/admin/site/{name}")
+    public @ResponseBody void putSite(@PathVariable String name){
+        SitesEntity pattern = new SitesEntity();
+        pattern.setName(name);
+        repository.add(pattern);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/admin/keyword/{name}/{personId}")
+    public @ResponseBody void putKeyWord(@PathVariable String name, @PathVariable Integer personId){
+        KeywordsEntity pattern = new KeywordsEntity();
+        pattern.setName(name);
+        pattern.setPersonId(personId);
+        repository.add(pattern);
+    }
+    //////////////////////////////////////POST
+    @RequestMapping(method = RequestMethod.POST, path = "/admin/person/{id}/{name}")
+    public @ResponseBody void postPerson(@PathVariable Integer id, @PathVariable String name){
+        PersonsEntity pattern = new PersonsEntity();
+        pattern.setId(id);
+        pattern.setName(name);
+        repository.update(pattern);
+    }
+    @RequestMapping(method = RequestMethod.POST, path = "/admin/site/{id}/{name}")
+    public @ResponseBody void postSite(@PathVariable Integer id, @PathVariable String name){
+        SitesEntity pattern = new SitesEntity();
+        pattern.setId(id);
+        pattern.setName(name);
+        repository.update(pattern);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/admin/keyword/{id}/{name}")
+    public @ResponseBody void postKeyWords(@PathVariable Integer id, @PathVariable String name){
+        KeywordsEntity pattern = new KeywordsEntity();
+        pattern.setId(id);
+        pattern.setName(name);
+        repository.update(pattern);
+    }
+
 
     /*
     @RequestMapping(method = RequestMethod.PUT, path = "/admin/person/{id}")
