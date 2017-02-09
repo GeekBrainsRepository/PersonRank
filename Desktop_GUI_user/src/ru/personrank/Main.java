9a893a6276281579629d921b1f9c6d7c30c1fc0e
Fyodor Mitkov <mitcovfn@gmail.com>
2017-02-09 11:19:42 +0200

    Добавлена авторизация пользователя

--- Desktop_GUI_user/src/ru/personrank/Main.java
+++ Desktop_GUI_user/src/ru/personrank/Main.java
@@ -2,7 +2,12 @@ package ru.personrank;
 
 import java.awt.Font;
 import java.awt.FontFormatException;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Enumeration;
 import javax.swing.*;
 import javax.swing.plaf.FontUIResource;
@@ -13,6 +18,8 @@ import ru.personrank.view.Window;
 
 public class Main {
 
+    private static final String URL_GET_AUTH = "http://37.194.87.95:30000/authentication/";
+
     public static void main(String[] args) {
 
         setLookAndFeel("Nimbus");
@@ -21,9 +28,9 @@ public class Main {
 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+
                 showLoginWindow();
             }
-
         });
     }
 
@@ -96,14 +103,42 @@ public class Main {
 
         @Override
         public boolean authenticate(String name, char[] password,
-                String server) throws Exception {
+                                    String server) throws Exception {
 
-            if (name.equalsIgnoreCase("user") && String.valueOf(password).equalsIgnoreCase("user")) {
+            if (chechUserLogin(name, String.valueOf(password))) {
                 Window.getInstance().setVisible(true);
                 return true;
             } else {
                 return false;
             }
+        }
+
+        private boolean chechUserLogin(String user, String login) {
+            URL url = null;
+            BufferedReader in = null;
+            try {
+                url = new URL(URL_GET_AUTH + user + "/" + login);
+                URLConnection urlConnection = url.openConnection();
+                in = new BufferedReader(new InputStreamReader(
+                        urlConnection.getInputStream()));
+                String inputLine;
+                inputLine = in.readLine();
+                if (inputLine.equals("true")) {
+                    return true;
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return false;
         }
     }
 
