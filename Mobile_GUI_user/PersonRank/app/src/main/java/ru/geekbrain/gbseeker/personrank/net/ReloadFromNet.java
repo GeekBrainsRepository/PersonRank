1934d734a32493733b41ee95b25b99c02a0d8579
Evgeny <evgenij.martynov92@gmail.com>
2017-02-15 20:12:56 -0800

    Удалены лишние папки и файлы

--- Mobile_GUI_user/PersonRank/app/src/main/java/ru/geekbrain/gbseeker/personrank/net/ReloadFromNet.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrain.gbseeker.personrank.net;
-
-/**
- * Created by m on 10.02.2017.
- */
-public interface ReloadFromNet {
-    void reload();
-}
--- Mobile_GUI_user/PersonRank/app/src/main/java/ru/geekbrain/gbseeker/personrank/net/iNet2SQL.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrain.gbseeker.personrank.net;
-
-public interface iNet2SQL {
-    void updateDB(String json,String param);
-    void updateUI();
-    String getInfo();
-    void init();
-}
