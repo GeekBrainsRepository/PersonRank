900305d6b9a7f496940c304d77a2775496fe00c6
Ivunin Viktor <vik_ivy@mail.ru>
2021-08-15 11:28:06 +0300

    Update Downloader.java

--- Crawler/src/main/java/Downloader.java
+++ Crawler/src/main/java/Downloader.java
@@ -9,7 +9,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Downloader {
-    //Замаскируемся под браузер Mozilla
     private static final String USER_AGENT =
             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
     private List<String> links = new LinkedList<String>();
